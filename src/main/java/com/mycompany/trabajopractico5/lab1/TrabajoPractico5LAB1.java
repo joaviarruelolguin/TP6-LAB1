e8be87a62a7c3bc8712b53f01a07366536b60f9b
Joaquín <joa.viarruel@gmail.com>
2026-09-15 19:16:42 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/trabajopractico5/lab1/TrabajoPractico5LAB1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajopractico5.lab1;
+
+/**
+ *
+ * @author jotil
+ */
+public class TrabajoPractico5LAB1 {
+
+    public static void main(String[] args) {
+     
+    }
+}
